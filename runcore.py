35b109bf999b6ddf29ec12b44206a55d662e7be6
chelseah <huangxpku@gmail.com>
2013-01-30 14:44:48 -0500

    runcore can change valuables in the observable.h and do iteration through the parameter space.

--- runcore.py
+++ runcore.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 import os
 import datetime as dt
-def gen_observable(infile):
+import numpy as np
+import scipy as sp
+def gen_observable(infile,newpara):
     fin = open(infile,mode='r')
     key = []
     value = []
     for line in fin.readlines():
         if (not line.startswith('#')):
             tempkey,tempvalue=line.rstrip().split('=')
-            key.append(tempkey)
-            value.append(tempvalue)    
+            key.append(tempkey.strip())
+            value.append(tempvalue.strip())    
     fin.close()
     para_dic=dict(zip(key,value))
+    for key,value in newpara.items():
+        print key,value
+        if(key in para_dic):
+            print key,value
+            para_dic[key]=value
     outfile = 'observable.h'
     fout = open(outfile,mode='w')
     fout.write('#ifndef OBSERBABLE_H\n')
@@ -26,11 +33,16 @@ def gen_observable(infile):
 def main():
     now = str(dt.datetime.utcnow())
     infile = 'observable_example.txt'
-    timestamp = now.split()[0]+'-'+'-'.join(now.split()[1].split(":")[0:2])
-    gen_observable(infile)
+    vcarray = np.linesapce(0.5,2,10)
+    perturb = ['vc']
+    #vcarray = np.array([1.5e3])
+    for vc in vcarray: 
+      timestamp = now.split()[0]+'-'+'-'.join(now.split()[1].split(":")[0:2])
+      newpara = dict(zip(perturb,[vc]))
+      gen_observable(infile,newpara)
     #print './core %s' % timestamp
-    os.system('Make clean')
-    os.system('Make')
-    #os.system('./core %s' % timestamp)
+      os.system('Make clean')
+      os.system('Make')
+      os.system('./core %s' % timestamp)
 if __name__=='__main__':
     main()
