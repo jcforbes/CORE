dbcd5567b55294d7b774834e15414818d3bbdad3
chelseah <huangxpku@gmail.com>
2013-07-07 15:25:14 -0400

    add in simple documentation in coag.cpp

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 CORE
 ====
 
-1d starformation code 
+1d starformation code
+------------------------
+Original version by Huang, X. C.; current working version by 
+Zhou, T. T. ;
+References: 
+\bibitem[Huang et al.(2013)]{2013ApJ...769...23H} Huang, X., Zhou, T., 
+\& Lin, D.~N.~C.\ 2013, \apj, 769, 23 
+Zhou,T., Huang, X. C. , Lin, D.~N.~C.,  et al. (2013) submitted to apjl
+
+Usage
+-----------------------
+
+Function Devision
+-----------------------
+coag: the contribution to the mass distributino from coagulation;
+
+conden: the contribution to the mass distribution from condensation;
+
+evap: the contribution to the mass distribution from evapration;
+
+frag: the contribution to the mass distribution from the fragmenation due 
+to KH instability;
+
+sf: the contribution to the mass distribution from star formation.
--- coag.cpp
+++ coag.cpp
@@ -1,32 +1,47 @@
 #include "coag.h"
 int coagulation(int i, int j, double ni, double nj, double ri, double rj)
+/*Input: i, j the mass bin index of the two cores coagulate; 
+ *       ni, nj the number of cores in each bins; 
+ *       ri, rj the radius coresponding to the mass for cores in each bins;
+ *
+ *Output: In later version, we shall change all the output so that they 
+ *        can be told from read the function call;
+ *        List as below: the global valuables 
+ *        delta_n
+ *        delta_Ek
+ *        ghost_mass 
+ *        are updated during the function call.
+ * */
 {
 	double z, temp, tempEk, tempenergy;
 	double coe1, coe2, coe;
-    if(m[i] > mBE)
+  /*assign sticking coefficients for each core*/
+  if(m[i] > mBE)
 	{
 		coe1 = pow((double)m[i]/mBE, beta);
-	}
-	else
+	} else {
 		coe1 = 1;
+  }
 	if(m[j] > mBE)
 	{
 		coe2 = pow((double)m[j]/mBE, beta);
 	}
-	else
+	else {
 		coe2 = 1;
+  }
+
 	coe = 1.0/(1 + coe1) + 1.0/(1 + coe2);
-//	coe *= 20;                                                                         //7.17_4 cancel
-//	z = 3 * ni * nj * vc * pow((ri + rj),2) * ((double)Timestep / (4 * pow(Rb,3))) ;    /*collision number*/
-	z = 3 * ni * nj * (v_d[i] + v_d[j]) * pow((ri + rj),2) * ((double)Timestep / (4 * pow(Rb,3)))/2 ;
+	  
+  z = 3 * ni * nj * (v_d[i] + v_d[j]) * pow((ri + rj),2) * ((double)Timestep / (4 * pow(Rb,3)))/2 ;
 	//below use gravitational focusing from Binney & Germaine:
-//	z *= coe;
-	coe = pow(v0,2.0)/2.0/Drho/pow(vc,2.0)*(pow(m[i],2.0/3.0)+pow(m[j],2.0/3.0))/pow(mBE,2.0/3.0);
+	
+  coe = pow(v0,2.0)/2.0/Drho/pow(vc,2.0)*(pow(m[i],2.0/3.0)+pow(m[j],2.0/3.0))/pow(mBE,2.0/3.0);
 	coe += 1;
 	z *= coe;
-
-	if(i == j)
-	{
+  /*record the reduce of core numbers and energy in bin i and j due to this 
+   * coagulation*/
+	if(i == j){
+    /*If coag with the same bin, need special care of the energy change*/
 		z = (double)z/2;
 		delta_n[i] -= z * 2;
 		tempenergy = z*m[i]*pow(v_d[i],2);
@@ -45,24 +60,25 @@ int coagulation(int i, int j, double ni, double nj, double ri, double rj)
 		tempEk += tempenergy;
 	}
 
-	temp = m[i] + m[j];
+	temp = m[i] + m[j]; /*the result mass assuming no mass loss during the 
+                       coagulation*/
+  /*if it ends up in our ghost zone*/
 	if(temp >= m[100])
 	{
 		ghost_mass += z * (m[i] + m[j]);
 	}
 	else
 	{
+    /*otherwise, it ends up in bin k*/
 		int k;
-        k = (i >= j)?i:j;
-		while(temp > m[k] && k < 99)
+    k = (i >= j)?i:j;
+		while(temp > m[k] && k < 99) /*search for bin k given the mass*/
 		{
 			k++;
 		}
+    /*Record the mass change in bin k due to this coagulation*/
 		delta_n[k] += z * temp/m[k];
 		delta_Ek[k] += tempEk;
-//		delta_n[k] += (temp - m[k-1])/(m[k] - m[k-1]) * z * temp/m[k];
-//		k--;
-//		delta_n[k] += (temp - m[k])/(m[k+1] - m[k]) * z * temp/m[k];
 	}
 	return 1;
 };
