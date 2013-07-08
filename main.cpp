6f74dcc2486dda416074d1208702300e8142d151
chelseah <huangxpku@gmail.com>
2013-07-07 20:50:07 -0400

    add in major comments in main

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 {
   char f_txt[50],f_rst[50];
   int rstflag;
+  //parsing the command line
   if (argc<2){
     printf("USAGE: %s <data> [<><>]\n",argv[0]);
     exit(1);
@@ -23,10 +24,12 @@ int main(int argc, char *argv[])
     switch (argc)
     {
       case 2:
+        /*start with initialization*/
         strcpy(f_txt,argv[1]);
         rstflag=0;
         break;
       case 3:
+        /*start with restart files*/
         strcpy(f_txt,argv[1]);
         strcpy(f_rst,argv[2]);
         rstflag=1;
@@ -37,18 +40,19 @@ int main(int argc, char *argv[])
     }
   }
   
-  start = clock();
+  start = clock(); /*program timing*/
 	int    i = 0, j = 0, k = 0;
-	double mytime = 0, TimeContinue = 0, time1;
+	double mytime = 0, TimeContinue = 0, time1; /*set up time*/ 
 	int ff = 0, ff2 = 0;
 	int countcolumn = 0, countcolumncf = 0;
 	double result[15][100], result_star[15][100], Ek_evo[15][100], frag[15][100], coag[15][100];  //result[0] stores the initial distribution; frag[] & coag[] store the separate contributions;
 
-    /***************initialize*************/
+  /***************initialize*************/
 	printf("initialize...\n");
   initialize(&mytime, &TimeContinue, &ff, &ff2,rstflag,f_rst);
 	time1 = (double)mytime/myr;
-  if(TimeContinue==0)
+  if(TimeContinue==0) 
+  /*If start from the very beginning, set everything to zero*/
 	{
 		for(i = 0; i<100; i++)
 		{
@@ -61,34 +65,39 @@ int main(int argc, char *argv[])
 	}
 	/**********************************/
 
-	printf("openfile...,ftxt=%s\n",f_txt);
+	printf("openfile...,ftxt=%s to save all the initial set ups\n",f_txt);
 	/************open file****************/
 	char f_distr[100], f_mass[100], f_star[100], cp[100], f_Ek[100], filefrag[100], filecoag[100];
 	FILE *fp, *fp1;
+  /*get all the output filenames*/
 	filename_gen(f_txt, f_distr, f_mass, f_star, f_Ek, cp, filefrag, filecoag);
-	fp = fopen(f_txt,"w");
+	fp = fopen(f_txt,"w"); 
+  /*f_txt: file to save initial condition.
+   *f_distr: file to output distributions.
+   *f_mass: file to output masses in each component at different time step.
+   *f_star: file to output stars in each bin.
+   *f_Ek: file to output energy in each bin.
+   *cp: 
+   *filefrag: file to output fragment contribution in each bin.
+   *filecoag: file to output coagulation contribution in each bin. */
 	print_paras(fp, back_mass, TimeContinue, total_mass_core,f_rst);
 	fp1=fopen(f_mass, "w");
 	fprintf(fp1, "#time(myr)\tstar mass\tcore mass\tghost mass\tchange in rhob\ttotal mass\ttotal Ek(10^40)\n");
-//  total_mass_core = 0;
-//  for (i = 0; i<100; i++){ 
-//    total_mass_core += n[i] * m[i];
-//  }
   fprintf(fp1, "%f\t%f\t%f\t%f\t%f\t%f\t%f\n", time1, star_mass/Msolar, total_mass_core/Msolar, ghost_mass/Msolar, (rhob-Rhob)/Rhob, total_mass/Msolar, totEk/pow(10,40));
 	/******************************/
 
 	printf("begin iteration...\n");
 	/**************iteration**************/
+  /*ff, ff2 are two counters in charge of when to dump data in the files*/
   double tempdelta[100]; 
   do
   {
 		ff2++;
-		//conden_nucle();
     for (i = 0; i< 100; i++){
       delta_n_coag[i] = 0;
       delta_n_frag[i] = 0;
     }
-		for( i = 0; i < 100; i++)
+		for( i = 0; i < 100; i++) //coagulate each bin
 		{
 			for(j = i; j < 100; j++)
 			{
@@ -96,30 +105,24 @@ int main(int argc, char *argv[])
 			}
 		}
     for ( i = 0 ; i<100; i++){
-      delta_n_coag[i] = delta_n[i];
-      star_formation(i);
+      delta_n_coag[i] = delta_n[i]; //record the change due to coagulation.
+      /*I believe We do starformation differently later on, usually set 
+       * star formation really small so it doesn't matter much here*/
+      star_formation(i); /*switches between different type of starformations*/
 		//	star_formation_binary(i);
 		//	star_formation_hierarchy(i);
     }
-//		for(i = 0; i<100; i++)
-//		{
-//			coag[countcolumncf][i] += delta_n_coag[i];
-//		}
-  	for (i = 0; i<100; i++){
+    //do fragmentation and record the change due to it;
+    for (i = 0; i<100; i++){
       tempdelta[i] = delta_n[i];
     }
 		for(i = 0; i<100; i++)
 		{
-//			if (m[i] < mBE)
 				fragmentation(i);
 		}
     for (i= 0;i<100;i++){
       delta_n_frag[i] = delta_n[i]-tempdelta[i];
     }
-//		for(i = 0; i<100; i++)
-//		{
-//			frag[countcolumncf][i] += delta_n_frag[i];
-//		}
 		if(ff2 == unit)
 		{
 			ff2 = 0;
@@ -127,7 +130,7 @@ int main(int argc, char *argv[])
 		}
 		evaporation();
 		conden_nucle();
-		update();
+		update(); //update the recorded changes
 		mytime += Timestep;
 		ff++;
     if(ff >= unit)
@@ -159,11 +162,14 @@ int main(int argc, char *argv[])
 
 /**************functions***************/
 int examine(double *n, double *star_mass, double *back_mass, double *total_mass, double *total_mass_core)
+/*CH: I dont know what TT intended to do here, need to ask.*/
 {
 	return 1;
 };
 
+
 double *r_gen(double *m, double *dens)               
+/*compute radius for each mass bin*/
 {
 	double *r, temp;
 	r = (double *)malloc(sizeof(double) * 100);
@@ -177,6 +183,8 @@ double *r_gen(double *m, double *dens)
 }
 
 double *dens_gen(double *m)
+/*density for cores, right now we are assume uniform density profile 
+ * independent of core masses.*/
 {
 	double *dens;
 	dens = (double *)malloc(sizeof(double) * 100) ;
@@ -190,58 +198,56 @@ double *dens_gen(double *m)
 }
 
 int initialize(double *mytime, double *TimeContinue, int *ff, int*ff2, int rstflag, char *f_rst)
+/*Initialize the zero time profile*/
 {
 	int i, j;
+  /*a log profile in the mass bin set ups*/
 	double a = 1.07897;//a=exp((log(100)-log(0.05))/2)=m[i+1]/m[i];
 	double A,temp=0;
 //	char flag;
 	
 //constants initialize:
-	m[0] = 0.05 * Msolar;
-	sum[0] = 0;
-	m_insolar[0] = (double)m[0]/Msolar;
+	m[0] = 0.05 * Msolar; 
 	m[100] = 100 * Msolar;
-	delta_n[0] = 0;
-	delta_n_coag[0] = 0;
-	delta_n_frag[0] = 0;
-	delta_n_cond[0] = 0;
-	delta_n_evap[0] = 0;
-  temp+=pow(m[0],1.0-alpha_init);
-	for( i = 1; i < 100; i++)
-	{ 
-		m[i] = m[i-1] * a;              
-		m_insolar[i] = (double)m[i]/Msolar;
-		sum[i] = sum[i-1] + m[i-1];
-		delta_n[i] = 0;
-		delta_n_coag[i] = 0;
-		delta_n_frag[i] = 0;
-		delta_n_cond[i] = 0;
-		delta_n_evap[i] = 0;
+  /*the smallest bin is always 0.05 Msolar and the biggest bin is always 
+   * 100 Msolar, need to allow user have a handel on this later on*/
+	sum[0] = 0;
+	m_insolar[0] = (double)m[0]/Msolar;  /*have a more convenient mass*/
+
+  /*initialize all the changes as zero*/
+  for (i=0;i<100;i++){
+	  delta_n[i] = 0;
+  	delta_n_coag[i] = 0;
+	  delta_n_frag[i] = 0;
+	  delta_n_cond[i] = 0;
+	  delta_n_evap[i] = 0;
 		n_star[i] = 0;
-		for(j = 0; j<4; j++)
+  	for(j = 0; j<4; j++)
 		{
 			rk[j][i] = 0;
 		}
-    if(i<bin){
-      temp+=pow(m[i],1.0-alpha_init);
+  }
+  temp+=pow(m[0],1.0-alpha_init);
+	for( i = 1; i < 100; i++)
+	{ 
+    /*compute masses for each bin*/
+		m[i] = m[i-1] * a;   
+		m_insolar[i] = (double)m[i]/Msolar;
+		sum[i] = sum[i-1] + m[i-1];
+
+	  if(i<bin){
+      temp+=pow(m[i],1.0-alpha_init); /*compute the power-law coeffs.*/
     }
   }
-//	A = Totalmass_core0/pow(m[0],1.0-alpha_init);
-//	A = A*(1-pow(a,1.0-alpha_init));
-//	A = A/(1-pow(a,(1.0-alpha_init)*(bin+1)));
-  A = Totalmass_core0/temp;
-  n_star[0] = 0;
-//interaction:
-//    printf("Use the default initial settings? y/n:\n");
-//	char flag = 'n';
-//	flag = getchar();
-//  flag = 'y';
+  A = Totalmass_core0/temp;/*make the total mass add up.*/
   if (rstflag)
 	{
 		readdata(mytime, TimeContinue, ff, ff2,f_rst);
-		n_star[0] = 0;/// just for follow 0513-11-cp.txt
-		total_mass_core = 0;
+	  /*making new stars, for old stars doesn't feedback.*/
+    n_star[0] = 0;
+    total_mass_core = 0;
 		totEk = 0;
+    /*set up other things as the file we restart from.*/
 		for(i = 0; i<100; i++)
 		{
 			Ek[i] = 0.5*m[i]*n[i]*v_d[i]*v_d[i];
@@ -294,7 +300,10 @@ int initialize(double *mytime, double *TimeContinue, int *ff, int*ff2, int rstfl
 
 	return 1;
 };
+
+
 int update()
+/*update for the current timestep*/
 {
 	int i;
 	total_mass = 0;
@@ -308,9 +317,6 @@ int update()
 		Ek[i] += delta_Ek[i];
 		totEk += Ek[i];
 		delta_Ek[i] = 0;
-// whether update the v(m)??
-//		if(n[i] != 0)
-//			v_d[i] = sqrt(2*Ek[i]/m[i]/n[i]);
 	}
 	double temp_rhob = 3 * back_mass/4/Pi/pow(Rb,3);
 	double tempratio = temp_rhob/rhob;
